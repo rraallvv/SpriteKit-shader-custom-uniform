5223bc9fd86e83d75f02021d0dc0fefc77a71d6a
Developer <>
2015-07-02 17:28:35 -0430

    Initial commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+
+//  Copyright (c) 2015 developer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet SKView *skView;
+
+@end
